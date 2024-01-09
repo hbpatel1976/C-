a2643806aa9afe887199e739d01785404ff43c56
Prof. (Dr) Hiren Patel <hbpatel1976@gmail.com>
2024-01-09 11:56:23 +0530

    Add files via upload

---
+++ obj05.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#define total 3
+class Room
+{
+	private: 
+		float length, width;
+	public:
+		void read();
+		void showArea();
+};
+void Room::read()
+		{
+			cout << "Enter Length : "; cin >> length;
+			cout << "Enter Width : "; cin >> width;
+		}
+void Room::showArea() 
+        {cout << " Area : " << length * width << endl;}
+		
+int main()
+{
+	int i;
+	Room r[total];
+	for(i=0; i<total; ++i){r[i].read();}
+	for(i=0; i<total; ++i){r[i].showArea();}
+}
